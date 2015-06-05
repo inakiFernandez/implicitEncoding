81ff658fcf3fe0b8ecd93daefaa66c4c10c75288
Inaki Fernandez <inaki.fernandez@loria.fr>
2015-06-05 16:50:22 +0200

    main.py operationa; mu/mu,lambda - ES implemented. Being tested now

--- src/main/mulambdaES.py
+++ src/main/mulambdaES.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
 #pylint: disable=pointless-string-statement
 from __future__ import division
 """
     Launcher, entry point for the pleiotropy experiments
 """
 #TODO (lambda + mu) - ES
+#TODO check for memory leaks (due to NN/genomes copy)
 #TODO check imports are useful. Static code analysis.
 #TODO To uncomment following lines
 #from ea.genome import Genome
@@ -26,12 +29,11 @@ import matplotlib.pyplot as plt
 #from os.path import isfile, join
 #from scipy.stats import *
 #from pylab import *
+
+########################Plotting params and functions##########################
 import brewer2mpl
 bmap = brewer2mpl.get_map('Set2', 'qualitative', 7)
 colors = bmap.mpl_colors   
-
-###############################################################################
-###############################################################################
 def perc(data_l):    
     data = np.asarray(data_l)
     median = np.zeros(data.shape[1])
@@ -42,7 +44,6 @@ def perc(data_l):
         perc_25[i] = np.percentile(data[:, i], 25)
         perc_75[i] = np.percentile(data[:, i], 75)
     return median, perc_25, perc_75
-
 
 def plot_one_curve(data, color, axis, label, quartiles=False):
     med, perc_25, perc_75 =  perc(data)
@@ -63,17 +64,14 @@ def plot_one_curve(data, color, axis, label, quartiles=False):
     for spine in axis.spines.values():
         spine.set_position(('outward', 5))
     axis.set_axisbelow(True)
-
+#########################NN structure definitions##############################
 def fullConnectionNet(nbIn, nbOut, nbHid, neurPerHid, bias):
-    
     net = FeedForwardNetwork()
     
     for i in xrange(nbIn):
         net.addInputModule(LinearLayer(1,name="i" + str(i)))        
-    
     for i in xrange(nbOut):
         net.addOutputModule(SigmoidLayer(1,name="o" + str(i)))
-    
     for i in xrange(nbHid):
         for j in xrange(neurPerHid):
             net.addModule(
@@ -95,8 +93,7 @@ def fullConnectionNet(nbIn, nbOut, nbHid, neurPerHid, bias):
                 FullConnection(
                 net["i" + str(i)],
                 net["h(0-" + str(j) + ")"] ,
-                name = "i" + str(i) + "-h(0-" + str(j) + ")"))
-        
+                name = "i" + str(i) + "-h(0-" + str(j) + ")"))        
         for k in xrange(nbHid - 1):
             for i in xrange(neurPerHid):
                 for j in xrange(neurPerHid):    
@@ -105,8 +102,7 @@ def fullConnectionNet(nbIn, nbOut, nbHid, neurPerHid, bias):
                     net["h(" + str(k) + "-" + str(i) + ")"], 
                     net["h(" + str(k + 1) + "-" + str(j) + ")"],
                     name = "h(" + str(k) + "-" + str(i) + ")-h(" 
-                        + str(k + 1) + "-" + str(j) + ")"))
-            
+                        + str(k + 1) + "-" + str(j) + ")"))            
         for i in xrange(neurPerHid):
             for j in xrange(nbOut):
                 net.addConnection(
@@ -127,120 +123,41 @@ def fullConnectionNet(nbIn, nbOut, nbHid, neurPerHid, bias):
             net.addConnection(
             FullConnection(b,
                            net["o" + str(i)],
-                           name = "b-o" + str(i)))
-    
+                           name = "b-o" + str(i)))    
     net.sortModules()    
-    
     return net
-
+    
+def decode(g, i, o, nbH, nPerH, b):
+    n = fullConnectionNet(i, o, nbH, nPerH, b)    
+    for i in xrange(n.paramdim):
+        n.params[i] = g[i]
+    n.reset()
+    return n
+    
 ###############################################################################
     
 def evaluate(indiv, pbDb, labelDb):
     fit = 0
     for i in xrange(len(pbDb)):
-        answer = round(indiv.activate(pbDb[i]),0)
-        #TODO Compute Error to be minimized (difference, or quadratic difference)
-        #régression symbolique
-        fit += (answer == labelDb[i])    
-    #Balance fitness w.r.t. proportion of same-valued answers:
+        answer = indiv.activate(pbDb[i])
+        #Quadratic error
+        fit += (answer - labelDb[i])**2    
+    #?Balance fitness w.r.t. proportion of same-valued answers:
     #e.g. 99 True, 1 False
-    return fit/float(len(pbDb)) # accuracy
+    return -fit
 
-#TODO Check all selection methods
-def select(p, f, selM, nbIndiv, tSize, elit):
-    #Pass into global.py or config.py
-    selMethods = {"RW": roulette,
-              "RB": rankBased,
-              "Tourn": tournament,
-              "Best": best,
-              }
-    #TODO try factorised version like: (sortedPop, sortedF) = 
-              #[(x, y) for (y,x) in sorted(zip(f,p), key=lambda pair: pair[0])] 
+def select(p, f, nbSel):
     sortedPop= [x 
         for (y,x) in sorted(zip(f,p), key=lambda pair: pair[0], reverse=True)]
-    sortedF = [y 
-        for (y,x) in sorted(zip(f,p), key=lambda pair: pair[0], reverse=True)]  
-    
-    parents = []
-
-    if elit:
-        parents.append(sortedPop[0])
-        sortedPop.remove(sortedPop[0])
-    
-    parents.extend(selMethods[selM](sortedPop, sortedF, nbIndiv, tSize)) 
+    #Truncation selection ","
+    parents = sortedPop[:mu]
            
-    return parents
+    return np.mean(parents, axis=1)
 
-def roulette(sPop, sF, nb, tSz):
-    result = []
-    total =  sum(sF)
-    prob =  sF / total
-    print prob
-    
-    n=0
-    #TODO debug random.choice for roulette
-    while n < nb:
-        result.append(np.random.choice(sPop, replace=True, p = prob))
-    return result
-
-def rankBased(sPop, sF, nb, tSz):
-    result = []
-    total = len(sPop)*(len(sPop) + 1) // 2 #n(n + 1)/2 
-    
-    rankWeights = range(1, len(sPop) + 1)
-    rankWeights = list(reversed(rankWeights))
-    
-    #Another version to use with random.choice()
-    #prob =  [x / total for x in rankWeights]
-    
-    n = 0
-    while n < nb:
-        dice = np.random.random()* total
-        index = 1
-        while dice > 0 and index <= len(sPop):
-            dice -= index 
-            index += 1
-        result.append(sPop[len(sPop) - (index - 1)])
-        n += 1
-    
-    return result
-
-def tournament(sPop, sF, nb, tSz):
-    result = []
-    #TODO Tournament
-    for i in xrange(nb):
-        #No replacement when sampling for tournament        
-        print sorted(np.random.sample(xrange(len(sPop)), tSz, replace = False))
-        #[sPop[i] 
-        #for i in 
-        #sorted(np.random.sample(xrange(len(sPop)), tSz, replace = False))][0]
-        #result.append()
-        
-    return result
-
-def best(sPop, sF, nb, tSz):
-    #TODO test
-    result = []
-    result.extend(sPop[0 : nb - 1]) 
-    return result
-
-
-def gnrtOffspring(par, pSz, pSurv, sig):
-    children = []    
-
-    if pSurv:
-        #TODO test
-        children.extend(par)
-
-    i = 0        
-    while len(children) < pSz:
-        children.append(mutate(par[i], sig))
-        #Loop through the parents in order. 
-        #This could be randomized or done otherwise
-        i = (i + 1) % len(par) 
-        
-    return children
-    
+def sampleOffspring(centr, l, sig, n):
+    return np.random.multivariate_normal(centr, np.identity(n) * sig, l)
+'''
+Deprecated    
 def mutate(indiv, sigma):
     result = indiv.copy()
 
@@ -248,7 +165,7 @@ def mutate(indiv, sigma):
         result.params[i] = np.random.normal(result.params[i], sigma)
     
     return result
-
+'''
 def task(vector,taskID):
     #TODO Pass into global.py or config.py
     tasks= {"AND": all,
@@ -280,169 +197,64 @@ def mux1(x):
         return x[np.ceil((len(x)-1)/2) + 1: len(x) - 1]            
 
 ###############################################################################
-
-percepts = 50
-bias = True
-out = 1
-nbHLayers = 1
-neurPerLayer = 3
-
+##############NN fixed structure definition - fully connected MLP##############
+percepts = 50; out = 1; nbHLayers = 1; neurPerLayer = 3; bias = True
+#Unnecessary if initialized centroid. 
+#Boundaries of initial weights. minWVal = -10; maxWVal = +10
 ###############################################################################
-
-pbDb = []
-labelDb = []
-pbSz = percepts
-dbSz = 200
-problemID = "MIN" # "MIN", "AND", "OR", "MAJ". 
+#########################Training DB construction##############################
+pbDb = []; labelDb = []; pbSz = percepts; dbSz = 200
+problemID = "MIN"#"MIN", "AND", "OR", "MAJ"
 #TODO TOFIX: "MUX1" (distance definitions in N-dimensional spaces needed)
-
 for i in xrange(dbSz):
     v = np.random.rand(pbSz)
     for i in xrange(pbSz):
         # Or random.choice( [0,1] )
         v[i] = round(v[i], 0) 
-    pbDb.append(v)
-    #Different tasks. 
+    pbDb.append(v)    
     #TODO Refactor into independent "task" module    
-    #labelDb.append(all(v)) #and function
     #Learn random associations (impossible task)
     #labelDb.append(round(np.random.rand(),0)) 
     labelDb.append(task(v, problemID))
-
-
-###############################################################################
-
-#Evo algo
-sigma = 0.5
-popSz = 8
-
-maxGen = 10
-#TODO Implemented: RB, Best; TOTEST/FIX: RW, Tourn (N)
-selection = "RB" 
-N = 2 # Tourn. size if Tourn(N) selected
-
-#Choose the best as parent #Maybe just make it survive as it is
-elitist = False 
-# Keep the parents as they are for the next population, 
-#then generated the remaining popSz - |parents| individuals to fill the pop.
-parSurv = False 
-
-#Percentage of individuals in population that are selected as parents
-#Other selection strategies can be considered
-selFactor = 0.6
-nbPar = int(np.ceil(selFactor * popSz))
-
-###############################################################################
+    
+##################Evolutionary algorithm (mu/mu, lambda)-ES####################
+sigma0 = 0.5; sigma = sigma0; lmbda = 20; mu = int(np.floor(lmbda / 2  ));
+maxGen = 10; bestEver = None #Best individual
+sigmas = [0]*(maxGen + 1); sigmas[0] = sigma
+########################Population initialization##############################
 #TODO Other methods for pop init. 
 #Implemented: fully connected MLP with 0-* hidden layers with the same number
 #of neurons per layer optional bias 
-pop = []
-for i in xrange(popSz):
-    pop.append(
-        fullConnectionNet( percepts, out, nbHLayers, neurPerLayer, bias))
-
-    
-fitness = [[0]*popSz for i in range(maxGen + 1)]
+auxNN = fullConnectionNet(percepts, out, nbHLayers, neurPerLayer, bias)
+spaceDim = auxNN.paramdim
+#Initial centroid of the distribution
+m0 = [0] * spaceDim; m = m0
+centroids = [[0]*spaceDim for i in range(maxGen + 1)]; centroids[0] = m
+#parents = (maxWVal - minWVal) * np.random_sample((mu, spaceDim)) - minWVal
+fitness = [[0]*lmbda for i in range(maxGen + 1)]
 
 #TODO TOCHECK termination conditions and data structures boundaries
-###############################################################################
-
-#Start Evo loop
-for g in xrange(maxGen):   
-    for ind in xrange(popSz):
-        fitness[g][ind] = evaluate(pop[ind], pbDb, labelDb)
+#TODO other stopping criteria    
+#############################Start Evo loop####################################
+for g in xrange(maxGen):       
     print "Generation: " + str(g)
-    #TODO check remaining selection and mutation/gnrtOffspring methods
-    parents = select(pop, fitness[g], selection, nbPar, N, elitist)
+    offspring = sampleOffspring(m, lmbda, sigma, spaceDim)
+    for ind in xrange(lmbda):
+        fitness[g][ind] = evaluate(
+                            decode(offspring[ind],
+                                   percepts, out, nbHLayers,
+                                       neurPerLayer, bias),
+                            pbDb, labelDb)
+    #TODO sort to keep bestEver        
+    #TODO check selection and mutation/sampleOffspring methods
+    m = select(pop, fitness[g], mu)
+    centroids[g + 1] = m
+    #TODO Update sigma and store it    
     
-    offspring = gnrtOffspring(parents, popSz, parSurv, sigma)    
-    pop = offspring
-
 print "Generation: " + str(maxGen)         
-
 #Last generation eval
 for ind in xrange(popSz):    
     fitness[maxGen][ind] = evaluate(pop[ind], pbDb, labelDb)
 
-#print fitness
-
 axis = plt.subplot2grid((1,1), (0,0))
-
 plot_one_curve(zip(*fitness), colors[0], axis, "Fitness", True)
-
-#print "Max final fitness: " + str(np.max(fitness[maxGen])) + ", "
-#print "Mean final fitness: " + str(np.mean(fitness[maxGen])) + ", "
-#print "Reachable fitness with systematic \"True\" answer: " 
-        #+ str((2**pbSz-1)/2**pbSz)
-
-###############################################################################
-
-"""
-g1 = Genome(9,2,0,0,100)
-
-print g1.getMaxConnections()
-
- """
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-means = [(-1,0),(2,4),(3,1)]
-cov = [np.diag([1,1]), np.diag([0.5,1.2]), np.diag([1.5,0.7])]
-alldata = ClassificationDataSet(2, 1, nb_classes=3)
-
-for n in xrange(400):
-    for klass in range(3):
-        input = multivariate_normal(means[klass],cov[klass])
-        alldata.addSample(input, [klass])
-
-tstdata, trndata = alldata.splitWithProportion( 0.25 )
-
-#trndata._convertToOneOfMany( )
-#tstdata._convertToOneOfMany( )
-
-print "Number of training patterns: ", len(trndata)
-print "Input and output dimensions: ", trndata.indim, trndata.outdim
-print "First sample (input, target, class):"
-print trndata['input'][0], trndata['target'][0]#, trndata['class'][0]
-
-fnn = buildNetwork( trndata.indim, 5, trndata.outdim, outclass=SoftmaxLayer )
-'''
-
-#testNN = buildNetwork( 16, 5, 4, 2, outclass=SoftmaxLayer, bias=True )
-#print testNN
-
-
-
