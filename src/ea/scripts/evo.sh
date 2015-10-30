7c05f50b73835689259aeae48ffd66b36ce8efdf
Inaki Fernandez <inaki.fernandez@loria.fr>
2015-10-30 14:56:28 +0100

    Parallelized execution of independent runs

--- src/ea/scripts/evo.sh
+++ src/ea/scripts/evo.sh
@@ -15,18 +15,20 @@ params="$1";
 let "nbrun= $2";
 
 out_global_dir=`echo $1 | awk 'BEGIN { FS= "[/.]"} ; { print $2}';`;
-echo $out_global_dir
+#echo $out_global_dir
 rm -rf logs/$out_global_dir
 
 mkdir logs/$out_global_dir
 
+out_parallel_file=`echo $out_global_dir.par`
+touch $out_parallel_file
 i=1
     while [ $i -le ${nbrun} ]; do
 	out_dir=`printf "out-%02d" $i`
 	rm -rf $out_dir
 	mkdir logs/$out_global_dir/$out_dir
-	python implicit_genome.py ${params} $out_dir
-	
+	#python implicit_genome.py ${params} $out_dir
+	echo "python implicit_genome.py ${params} $out_dir" >> $out_parallel_file
 
         #gzip a faire plus tard dans le programme (mtn je peux pas -> experiences en route)
 	#penser a supprimer l affichage texte du simulateur
@@ -38,8 +40,13 @@ i=1
     done
 #gzip -r "$out_global_dir/$out_dir"
 
+#mv $out_parallel_file $out_global_dir
+parallel -j 4 < $out_parallel_file
 mv logs/$out_global_dir $out_global_dir
+
 mkdir logs
+
+
 
 
 exit 0;
